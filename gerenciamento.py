a499f0447d91aea397c1e7d820c38b4a60e48cc3
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-12-01 19:11:59 -0300

    att gerenciador v1

--- gerenciamento.py
+++ gerenciamento.py
@@ -13,29 +13,28 @@ def gerenciamento(nome_arq):
                 bloco = partes[1]
             case "NUMERO":
                 var = "".join(partes[1:])
-                lexemas, valores = atribuicao(var)
+                lexemas, valores = declaracao(var)
                 tabela = []
                 for lexema, valor in zip(lexemas, valores):
-                    try: # verifica se o valor é numérico
-                        valor = float(valor)
-                        tabela.append({"lexema": lexema, "valor": valor, "tipo": "NUMERO", "bloco": bloco})
-                    except ValueError:
+                    if(tipo(valor) != "NUMERO"):
                         print(f"Erro (linha {count_linhas}): valor não numérico")
                         break
+                    else:
+                        tabela.append({"lexema": lexema, "valor": valor, "tipo": "NUMERO", "bloco": bloco})
                 if escopo_criacao(tabela, pilha):
                     pilha.append(tabela)
                 else:
                     print(f"Erro (linha {count_linhas}): variável já declarada")
             case "CADEIA":
                 var = "".join(partes[1:])
-                lexemas, valores = atribuicao(var)
+                lexemas, valores = declaracao(var)
                 tabela = []
                 for lexema, valor in zip(lexemas, valores):
-                    if valor[0] == '"' and valor[len(valor)-1] == '"':  # verifica se o valor é uma cadeia
-                        tabela.append({"lexema": lexema, "valor": valor, "tipo": "CADEIA", "bloco": bloco})
-                    else:
+                    if(tipo(valor) != "CADEIA"):
                         print(f"Erro (linha {count_linhas}): valor não é uma cadeia")
                         break
+                    else:
+                        tabela.append({"lexema": lexema, "valor": valor, "tipo": "CADEIA", "bloco": bloco})
                 if escopo_criacao(tabela, pilha):
                     pilha.append(tabela)
                 else:
@@ -52,7 +51,11 @@ def gerenciamento(nome_arq):
             case "\n":
                 pass
             case _:
-                print("atribuicao")
+                try:
+                    var, valor = linha.split("=")
+                    atribuicao(var, valor, pilha, count_linhas)
+                except ValueError:
+                    print(f"Erro (linha {count_linhas}): comando inválido")
         count_linhas += 1      
     
     
@@ -64,7 +67,7 @@ def lista_comandos(nome_arq):
     arquivo.close()
     return lista
 
-def atribuicao(var):
+def declaracao(var):
     lexemas = []
     valores = []
     atribuicoes = re.findall("[a-zA-Z][0-9a-zA-Z_]*=.+", var)
@@ -83,8 +86,28 @@ def escopo_criacao(tabela, pilha):
     return True
 
 def busca_variavel(variavel, pilha, linha):
-    for i in pilha[len(pilha)-1:0:-1]: # percorre a pilha de trás pra frente
+    for i in pilha[len(pilha)-1:0:-1]: # percorre a pilha do topo pra baixo
         for tabela in i:
             if tabela["lexema"] == variavel:
                 return tabela["valor"]
     raise Exception(f"Erro (linha {linha}): variável '{variavel}' não declarada")
+
+def atribuicao(variavel, valor, pilha, linha):
+    for i in pilha[len(pilha)-1:0:-1]: # percorre a pilha do topo pra baixo
+        for tabela in i:
+            if tabela["lexema"] == variavel:
+                if tabela["tipo"] == tipo(valor):
+                    tabela["valor"] = valor
+                else:
+                    raise Exception(f"Erro (linha {linha}): valor não é do tipo '{tabela['tipo']}'")
+                return
+    raise Exception(f"Erro (linha {linha}): variável '{variavel}' não declarada")
+
+def tipo(valor):
+    if valor[0] == '"' and valor[len(valor)-1] == '"':
+        return "CADEIA"
+    try:
+        float(valor)
+        return "NUMERO"
+    except ValueError:
+        return None
